37317b5b896fa4e90844d181e29bd51b1d9a6bfa
shoaibmeh <shoaibmehmood914@gmail.com>
2024-01-04 15:08:51 +0500

    first commit

---
+++ src/app/home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to Home</h1>
+        {/* Home page content */}
+      </div>
+    );
+  };
+  
+  export default Home;
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,266 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
 
-export default function Home() {
+export default function AuthPage() {
+  // toggle between signup && sign in
+  const toggleForm = () => {
+    setFormType(formType === "signin" ? "signup" : "signin");
+  };
+  const [formType, setFormType] = useState("signup");
+
+  // sign-in function.............
+  const [signInData, setSignInData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSignInChange = (e) => {
+    const { name, value } = e.target;
+    setSignInData({
+      ...signInData,
+      [name]: value,
+    });
+  };
+
+  // sign in function on submit...........
+  const handleSignInSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://127.0.0.1:8000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(signInData),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      // console.log('User signed in:', data);
+      setModalContent({ type: "success", message: "Sign-in successful!" });
+      window.location.href = "https://nextjs.org/";
+    } else {
+      // console.error('User sign-in failed');
+      setModalContent({ type: "error", message: "Invalid Email or Password." });
+    }
+  };
+
+  // registration function...........
+  const [signUpData, setSignUpData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignUpChange = (e) => {
+    const { name, value } = e.target;
+    setSignUpData({
+      ...signUpData,
+      [name]: value,
+    });
+  };
+
+  // sign up function on submit...........
+  const handleSignUpSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://127.0.0.1:8000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(signUpData),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("User registered:", data);
+      setModalContent({ type: "success", message: "Sign-up successful!" });
+      setFormType("signin");
+    } else {
+      if (response.status === 409) {
+        setModalContent({
+          type: "error",
+          message: "User with this email already exists.",
+        });
+      }
+    }
+  };
+
+  // modal function....................
+  const [modalContent, setModalContent] = useState(null);
+
+  const closeModal = () => {
+    setModalContent(null);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex justify-center items-center h-screen">
+      {/* sign-in form//////////////////// */}
+      {formType === "signin" && (
+        <form
+          onSubmit={handleSignInSubmit}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <h2 className="text-center mb-4 font-bold text-xl">Sign In</h2>
+          <h2 className="text-center mb-4  text-md">
+            Don't have an account?
+            <span
+              className="text-sm text-blue-500 hover:text-blue-800 focus:outline-none cursor-pointer"
+              onClick={toggleForm}
+            >
+              {" "}
+              Create one now.
+            </span>
+          </h2>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              required
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={signInData.email}
+              onChange={handleSignInChange}
             />
-          </a>
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              required
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              value={signInData.password}
+              onChange={handleSignInChange}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      )}
+
+      {/* sign-up form//////////////////// */}
+      {formType === "signup" && (
+        <form
+          onSubmit={handleSignUpSubmit}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <h2 className="text-center mb-4 font-bold text-xl">Sign Up</h2>
+          <h2 className="text-center mb-4  text-md">
+            Already have an account?
+            <span
+              className="text-sm text-blue-500 hover:text-blue-800 focus:outline-none cursor-pointer"
+              onClick={toggleForm}
+            >
+              {" "}
+              Sign in here.
+            </span>
+          </h2>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              required
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+              value={signUpData.name}
+              onChange={handleSignUpChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              required
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={signUpData.email}
+              onChange={handleSignUpChange}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              required
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              value={signUpData.password}
+              onChange={handleSignUpChange}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      )}
+
+      {/* Modal */}
+      {modalContent && (
+        <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-gray-800 bg-opacity-75">
+          <div className="bg-white p-8 rounded shadow-md flex flex-col items-center">
+            <p className="text-xl font-semibold mb-4">
+              {modalContent.type === "success" ? "Success!" : "Error!"}
+            </p>
+            <p>{modalContent.message}</p>
+            <button
+              onClick={closeModal}
+              className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Close
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+    </div>
+  );
 }
