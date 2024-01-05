d7562e2e8e361ca55bd97b85c8a5f336d85539cf
shoaibmeh <shoaibmehmood914@gmail.com>
2024-01-05 11:41:33 +0500

    second commit

---
+++ src/app/components/Dashboard.js
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+    return (
+      <div>
+        <h1>Welcome to Home</h1>
+        {/* Home page content */}
+      </div>
+    );
+  };
+  
+  export default Dashboard;
